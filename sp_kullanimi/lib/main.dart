5f278e8e1f55837c4abcc822e000094c1fd1b9db
Yusuf Emre Altun <67118611+HxH0D@users.noreply.github.com>
2023-11-23 18:10:25 +0300

    Update main.dart

--- sp_kullanimi/lib/main.dart
+++ sp_kullanimi/lib/main.dart
@@ -1,85 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sp_kullanimi/home_page.dart';
 
-class Anasayfa extends StatefulWidget {
-  const Anasayfa({super.key});
-
-  @override
-  State<Anasayfa> createState() => _AnasayfaState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _AnasayfaState extends State<Anasayfa> {
-  int sayac = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  void initState() {
-    super.initState();
-    // test();
-    sayacKontrol();
-  }
-
-  Future<void> sayacKontrol() async {
-    var sp = await SharedPreferences.getInstance();
-    sayac = sp.getInt("sayac") ?? 0; // Okuma
-
-    setState(() {
-      sayac = sayac + 1;
-    });
-
-    sp.setInt("sayac", sayac); // Kayıt
-  }
-
-  Future<void> test() async {
-    // Tanımlama
-    var sp = await SharedPreferences.getInstance();
-
-    // Veri Kaydı
-    sp.setString("ad", "Ahmet");
-    sp.setInt("yas", 23);
-    sp.setDouble("boy", 1.78);
-    sp.setBool("bekar", true);
-
-    var liste = <String>[];
-    liste.add("Ali");
-    liste.add("Ece");
-    liste.add("Veli");
-
-    sp.setStringList("liste", liste);
-
-    // Veri Silme
-    // sp.remove("ad");
-
-    // Veri Okuma
-    String gelenAd = sp.getString("ad") ?? "isim yok";
-    int gelenYas = sp.getInt("yas") ?? 0;
-    double gelenBoy = sp.getDouble("boy") ?? 0.0;
-    bool gelenBekar = sp.getBool("bekar") ?? false;
-    var gelenListe = sp.getStringList("liste") ?? <String>[];
-
-    print("Gelen ad    : $gelenAd");
-    print("Gelen yas   : $gelenYas");
-    print("Gelen boy   : $gelenBoy");
-    print("Gelen bekar : $gelenBekar");
-
-    if (gelenListe != null) {
-      for (var a in gelenListe) {
-        print("Gelen liste: $a");
-      }
-    }
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Shared Preferences Kullanimi"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      body: Center(
-        child: Text(
-          "Açılış Sayısı : $sayac",
-          style: const TextStyle(fontSize: 50),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
